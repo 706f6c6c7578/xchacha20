f9c6befd35c4178b6aa0a0b151af022f405a6eb8
Stefan <stefan.claas@mail.ru>
2024-09-07 22:28:46 +0200

    Add files via upload

---
+++ xchacha20.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"encoding/hex"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"strings"
+
+	"golang.org/x/crypto/chacha20"
+)
+
+func readHexFile(filename string) ([]byte, error) {
+	content, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, fmt.Errorf("error reading file: %v", err)
+	}
+
+	hexString := strings.TrimSpace(string(content))
+	decoded, err := hex.DecodeString(hexString)
+	if err != nil {
+		return nil, fmt.Errorf("error decoding hex: %v", err)
+	}
+
+	return decoded, nil
+}
+
+func printUsage() {
+	fmt.Fprintf(os.Stderr, `Usage: %s <keyfile> <noncefile> < infile > outfile
+
+XChaCha20 encryption/decryption tool
+
+Arguments:
+  keyfile    Path to the file containing the 32-byte key in hexadecimal format
+  noncefile  Path to the file containing the 24-byte nonce in hexadecimal format
+
+The program reads from stdin and writes to stdout. Use input/output redirection for files.
+
+Examples:
+  Encryption: %s key.hex nonce.hex < plaintext.txt > encrypted.bin
+  Decryption: %s key.hex nonce.hex < encrypted.bin > decrypted.txt
+
+Note: The key should be 64 hexadecimal characters long (32 bytes).
+      The nonce should be 48 hexadecimal characters long (24 bytes).
+`, os.Args[0], os.Args[0], os.Args[0])
+}
+
+func main() {
+	if len(os.Args) != 3 {
+		fmt.Fprintln(os.Stderr, "Error: Incorrect number of arguments")
+		printUsage()
+		os.Exit(1)
+	}
+
+	keyFile := os.Args[1]
+	nonceFile := os.Args[2]
+
+	key, err := readHexFile(keyFile)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error reading key file: %v\n", err)
+		os.Exit(1)
+	}
+
+	nonce, err := readHexFile(nonceFile)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error reading nonce file: %v\n", err)
+		os.Exit(1)
+	}
+
+	if len(key) != chacha20.KeySize {
+		fmt.Fprintf(os.Stderr, "Error: Invalid key size. Expected %d bytes (64 hex characters), got %d bytes\n", chacha20.KeySize, len(key))
+		os.Exit(1)
+	}
+
+	if len(nonce) != chacha20.NonceSizeX {
+		fmt.Fprintf(os.Stderr, "Error: Invalid nonce size. Expected %d bytes (48 hex characters), got %d bytes\n", chacha20.NonceSizeX, len(nonce))
+		os.Exit(1)
+	}
+
+	cipher, err := chacha20.NewUnauthenticatedCipher(key, nonce)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error creating cipher: %v\n", err)
+		os.Exit(1)
+	}
+
+	buf := make([]byte, 8192) // read/write buffer
+	for {
+		n, err := os.Stdin.Read(buf)
+		if err != nil && err != io.EOF {
+			fmt.Fprintf(os.Stderr, "Error reading input: %v\n", err)
+			os.Exit(1)
+		}
+		if n == 0 {
+			break
+		}
+
+		cipher.XORKeyStream(buf[:n], buf[:n])
+		
+		if _, err := os.Stdout.Write(buf[:n]); err != nil {
+			fmt.Fprintf(os.Stderr, "Error writing output: %v\n", err)
+			os.Exit(1)
+		}
+
+		if err == io.EOF {
+			break
+		}
+	}
+}
